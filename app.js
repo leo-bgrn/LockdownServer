26d3607bbac29577d5da4b9ae715cd77142afacf
BONGIORNO <bongiorno.leo@gmail.com>
2020-11-05 18:38:05 +0100

    Remove comments in app.js

--- app.js
+++ app.js
@@ -10,17 +10,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 10 * 60 * 1000,
+  max: 100,
 });
 
 app.use(limiter);
 
 app.use(bodyParser.json());
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
@@ -42,18 +41,13 @@ app.use("/questions", questionsRouter);
 const suggestionsRouter = require("./src/routes/suggestions.route");
 app.use("/suggestions", suggestionsRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
-  // res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.send(err.message);
 });
