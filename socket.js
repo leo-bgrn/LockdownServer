2eed9464b74f36dfa8e80dc1c527064324969852
BONGIORNO <bongiorno.leo@gmail.com>
2020-11-08 16:10:34 +0100

    Move socket to separate folder

---
+++ socket.js
@@ -0,0 +1,11 @@
+function socketBehavior(socket) {
+  console.log("Connected !");
+  socket.on("next", (data) => {
+    console.log(data);
+    socket.emit("question", { question: "my question" });
+  });
+}
+
+module.exports = {
+  socketBehavior,
+};
